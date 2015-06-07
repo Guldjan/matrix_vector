44c04356c949c528fb04a5a2b201f87246c5673a
Guldjan <guldjan.kupen@gmail.com>
2015-06-07 18:54:46 +0300

    Update

--- setup.py
+++ setup.py
@@ -1,23 +1,13 @@
-""" See:
-https://github.com/Guldjan/matrix-vector
-"""
 from setuptools import setup
 
 setup(
 	name='matrix_vector',
 	version='0.0.0',
 	description='A python package for matrices and vectors operations.',
-	url='https://github.com/Guldjan/matrix-vector',
+	url='https://github.com/Guldjan/matrix_vector',
 	author='Guldjan Kupen',
 	author_email='guldjan.kupen@gmail.com',
 	license='MIT',
-	classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License', 
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4'
-    ]
+	packages=['matrix_vector'],
+    zip_safe=False
 )
